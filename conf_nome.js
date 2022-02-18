59c9808bdd77511a916fc6e432fb722a8007a358
Dani Guerra <daniela.oliveira@alfalaval.com>
2022-02-18 13:00:14 -0300

    aprendendo usar git

---
+++ conf_nome.js
@@ -0,0 +1,2 @@
+// git config --local user.name "Dani Guerra"
+// git config --local user.email "guerra_dani@outlook.com"
---
+++ exer2.js
@@ -0,0 +1,4 @@
+// const nome = "Leo";
+// const idade = 23;
+// const bebidaMaior = "cerveja";
+// const bebidaMenor = "suco";
