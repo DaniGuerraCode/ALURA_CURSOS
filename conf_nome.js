18c52df566a1c191ca3682957eee502defacdfdf
Dani Guerra <guerrinha2008@hotmail.com>
2022-02-18 13:11:42 -0300

    aprendendo git

--- conf_nome.js
+++ conf_nome.js
@@ -1,2 +1,4 @@
 // git config --local user.name "Dani Guerra"
 // git config --local user.email "guerra_dani@outlook.com"
+// git log -p (mostra todas as alterações)
+// git log (mostra resumidamente as alterações)
